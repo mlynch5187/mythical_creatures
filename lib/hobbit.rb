ca3d1d2eaba15d0e13659c44f50f8c37ead2bfc4
Alex Lynch <mlynch5187@gmail.com>
2020-01-30 10:26:46 -0700

    Create celebrate_birthday and adult? methods in hobbit.rb

--- lib/hobbit.rb
+++ lib/hobbit.rb
@@ -1,9 +1,21 @@
 class Hobbit
-  attr_reader :name, :disposition
+  attr_reader :name, :disposition, :age
   def initialize(name, disposition = "homebody")
     @name = name
     @disposition = disposition
+    @age = 0
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def adult?
+    if @age > 32
+      true
+    else
+      false
+    end
+  end
 
 end
